b53a2238cb10ab51a7934ce24e697ee9c227d806
Niels <Nozols0@gmail.com>
2015-11-13 18:03:48 +0100

    first commit

---
+++ AutoSaveBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AutoSave
+{
+    class AutoSaveBehaviour : MonoBehaviour
+    {
+        public void Awake() {
+            DontDestroyOnLoad(this);
+        }
+
+        public void Start() {
+            StartCoroutine("AutoSave");
+        }
+
+        public void onGUI() {
+            GUI.Label(new Rect(Screen.width - 210, 10, 100, 100), "Autosaving game");
+            if (GameController.Instance.isSavingGame) {
+                GUI.Label(new Rect(Screen.width - 110, 10, 100, 100), "Autosaving game");
+            }
+        }
+
+        private IEnumerator AutoSave() {
+            for (;;) {
+                Debug.Log("Autosaving game.");
+                GameController.Instance.saveGame("Saves/Savegames/AutoSave-" + GameController.Instance.park.parkName + ".txt");
+                Debug.Log("Finished autosaving.");
+
+                yield return new WaitForSeconds(20);
+            }
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace AutoSave
+{
+
+    public class Main : IMod
+    {
+        private GameObject __go;
+
+        public string Description
+        {
+            get
+            {
+                return "Enables autosave";
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "AutoSave";
+            }
+        }
+
+        public void onDisabled()
+        {
+            UnityEngine.Object.Destroy(__go);
+        }
+
+        public void onEnabled()
+        {
+            __go = new GameObject();
+            __go.AddComponent<AutoSaveBehaviour>();
+        }
+    }
+}
