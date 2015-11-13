850b959d132c5b03aab569b725555fee2e2a983a
Niels <Nozols0@gmail.com>
2015-11-13 18:38:58 +0100

    Remove debug gui

--- AutoSaveBehaviour.cs
+++ AutoSaveBehaviour.cs
@@ -14,7 +14,6 @@ namespace AutoSave
         }
 
         public void onGUI() {
-            GUI.Label(new Rect(Screen.width - 210, 10, 100, 100), "Autosaving game");
             if (GameController.Instance.isSavingGame) {
                 GUI.Label(new Rect(Screen.width - 110, 10, 100, 100), "Autosaving game");
             }
